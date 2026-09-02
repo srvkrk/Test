0a507d75cfb0090515dacc29cb931618650624dd
SOURAV KARAK <37249717+srvkrk@users.noreply.github.com>
2026-09-02 22:42:33 +0530

    Delete cmitest.bat
    ok

--- cmitest.bat
+++
@@ -1,8 +0,0 @@
-set TC_ROOT=C:\Siemens\cmitest
-set FMS_HOME=C:\Siemens\cmitest\tccs
-set JAVA_HOME=C:\Siemens\Zulu_Java
-set JRE_HOME=C:\Siemens\Zulu_Java
-set CLASSPATH=%TC_ROOT%\portal
-set PATH=%FMS_HOME%\bin;%FMS_HOME%\lib;TC_ROOT\portal;%PATH%
-start "Start FCC" /min cmd /c "%TC_ROOT%\tccs\bin\fccstat.exe -restart"
-D:\TC14_3\eclipse\eclipse.exe -vm C:\Siemens\Zulu_Java\bin\javaw
